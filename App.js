a4528ddb34d96a3d6f416cb0b25f8b115ea6224f
Tim Wei <galadiya41@gmail.com>
2025-09-18 00:30:19 +0800

    test: 0.8.2

--- App.js
+++ App.js
@@ -9,19 +9,10 @@ import { View, Text, Button } from "react-native";
 
 const SAMPLE_RATE = 16000;
 
-/**
- * I setup the audio context and recorder in the beginning,
- * it causes runtime error: `SetProperty: RPC timeout. Apparently deadlocked. Aborting now.`
- * (only on iOS simulator, the real device works fine)
- */
 const Record1 = () => {
   const recorderRef = useRef(null);
   const [isRecording, setIsRecording] = useState(false);
 
-  /**
-   * Case 1:
-   * setup audio context then setup recorder
-   */
   useEffect(() => {
     (async () => {
       AudioManager.setAudioSessionOptions({
@@ -32,16 +23,11 @@ const Record1 = () => {
 
       AudioManager.requestRecordingPermissions();
 
-      // do some other stuff
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const _audioContext = new AudioContext();
-
-      // do some other stuff
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       recorderRef.current = new AudioRecorder({
         sampleRate: SAMPLE_RATE,
-        bufferLengthInSamples: SAMPLE_RATE * 2,
+        bufferLengthInSamples: SAMPLE_RATE * 1,
       });
 
       recorderRef.current.onAudioReady((event) => {
@@ -51,61 +37,6 @@ const Record1 = () => {
       });
     })();
   }, []);
-
-  /**
-   * Case 2:
-   * setup recorder then setup audio context
-   */
-  // useEffect(() => {
-  //   (async () => {
-  //     AudioManager.setAudioSessionOptions({
-  //       iosCategory: "playAndRecord",
-  //       iosMode: "spokenAudio",
-  //       iosOptions: ["defaultToSpeaker", "allowBluetoothA2DP"],
-  //     });
-
-  //     AudioManager.requestRecordingPermissions();
-
-  //     // do some other stuff
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  //     recorderRef.current = new AudioRecorder({
-  //       sampleRate: SAMPLE_RATE,
-  //       bufferLengthInSamples: SAMPLE_RATE * 2,
-  //     });
-
-  //     recorderRef.current.onAudioReady((event) => {
-  //       const { buffer, numFrames, when } = event;
-
-  //       console.log("Audio recorder buffer:", buffer.duration, numFrames, when);
-  //     });
-
-  //     // do some other stuff
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
-  //     const _audioContext = new AudioContext();
-  //   })();
-  // }, []);
-
-  const handlePlay = async () => {
-    const audioContext = new AudioContext();
-
-    const audioBuffer = await fetch(
-      "https://software-mansion.github.io/react-native-audio-api/audio/music/example-music-01.mp3"
-    )
-      .then((response) => response.arrayBuffer())
-      .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer));
-
-    const playerNode = audioContext.createBufferSource();
-    playerNode.buffer = audioBuffer;
-
-    playerNode.connect(audioContext.destination);
-    playerNode.start(audioContext.currentTime);
-
-    // do something...
-    await new Promise((resolve) => setTimeout(resolve, 9000));
-
-    playerNode.disconnect();
-  };
 
   const handleRecord = async () => {
     if (isRecording) {
@@ -134,7 +65,6 @@ const Record1 = () => {
           }
           onPress={handleRecord}
         />
-        <Button title="Play music" onPress={handlePlay} />
       </View>
     </View>
   );
